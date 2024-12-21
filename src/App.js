723a8827daa982a7944f8e9bb4e2a08393f74df1
RalphsCode <RalphsCode@gmail.com>
2024-12-20 21:41:44 -0800

    Initial project set-up.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Draw a card from a deck
+import './App.css';
+import DrawACard from './DrawACard';
+
+function App() {
+  return (
+    <div className="App">
+      <DrawACard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DrawACard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DrawACard = () => {
+    return (
+        <div>
+        <h1>"RalphsCode - Drawing a Card"</h1>
+        <img src='https://deckofcardsapi.com/static/img/back.png' />
+        </div>
+) }
+
+export default DrawACard;
